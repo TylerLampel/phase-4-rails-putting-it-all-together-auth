d022d5d5a31850c1bf525c414b1e14bed4aaa44a
Tyler Lampel <lampel13@gmail.com>
2023-01-01 11:25:43 -0500

    commit tree d0fb8a4f682955724175ef5fd2c4ecbf5ad12fe2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  resources :recipes, only: [:index, :create]
+
+  #signup
+  post "/signup", to: "users#create"
+  #auto-login on refresh(stay logged in)
+  get "/me", to: "users#show"
+  #log in
+  post "/login", to: "sessions#create"
+  #log out
+  delete "/logout", to: "sessions#destroy"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
